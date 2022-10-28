8ac13e8ff47a1ef095b002165f494810f17959ed
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-10-28 23:05:14 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES (NULL,21,"F","red","low",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("bear3",5,"M","blue","high",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("bear2",31,"F","black","low",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("bear4",10,"M","white","high",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("bear7",11,"F","red","low",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("bear8",22,"M","green","low",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("bear10",17,"F","yellow","high",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("bear5",15,"F","red","done",1);
